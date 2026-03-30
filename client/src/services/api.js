b8797097d8531801c73ffbe510c471357e609091
DiptanshuVishwa <diptanshuvishwa364@gmail.com>
2026-03-30 15:34:21 +0530

    Updated baseUrl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://golf-charity-platform-0prl.onrender.com/api",
 });
 
 export default API;
